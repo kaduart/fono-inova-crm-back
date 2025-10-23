6a81d8680c81bf751dea830c59b0c22a2d6982bf
kadu art <ricardosantos.ti15@gmail.com>
2025-10-23 17:19:29 -0300

    Adjust propmt AI

--- services/aiAmandaService.js
+++ services/aiAmandaService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import OpenAI from "openai";
 import { Readable } from "stream";
+import { POLICY_RULES } from "../utils/amandaPrompt";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
