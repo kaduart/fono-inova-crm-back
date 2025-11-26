2854b5c0246e0b616a4856fd42594641fb7129fe
kadu art <ricardosantos.ti15@gmail.com>
2025-11-26 13:16:56 -0300

    ajuste amanda e followups

--- config/bullConfig.js
+++ config/bullConfig.js
@@ -3,8 +3,5 @@ import { Queue, QueueEvents } from "bullmq";
 import chalk from "chalk";
 import { redisConnection } from "./redisConnection.js";
 
-redisConnection.on("connect", () => console.log(chalk.green("✅ Redis conectado")));
-redisConnection.on("ready", () => console.log(chalk.cyan("🧩 Redis pronto")));
-
 export const followupQueue = new Queue("followupQueue", { connection: redisConnection });
 export const followupEvents = new QueueEvents("followupQueue", { connection: redisConnection });
